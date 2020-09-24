c3e6b3c026aacd51caf2070a795f02e5ad65435a
aman <aman.khan.silawat.aks@gmail.com>
2020-09-24 15:20:04 +0530

    added momoization exercise

---
+++ 01_space_n_time_complexity.js
@@ -0,0 +1,35 @@
+/*
+
+What is an algorithm?
+algorithm is just the step that you take to solve the problem.
+
+** Time Complexity **
+ 
+Space complexity
+    how much memory us used?
+
+Time complexity
+    How many primitive poerations are executed?
+
+...with respect to input size
+...and assuming worst case scenarios
+
+
+constant    O(1)        SUPER FAST
+logarithmic O(log n)        |
+liner       O(n)            |
+quadratic   O(n^2)          ᐯ
+exponential O(k^n)     SUPER SLOW
+
+
+
+Bigo O Notation
+
+1 + 1 + 1                   :constant
+n * n                       :quardratic
+n * n * n                   :quardratic
+done work on half arr leng  :logarithmic
+
+
+breadcome method
+*/
