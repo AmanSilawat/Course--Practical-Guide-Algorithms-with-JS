c96e4cb26e651fbf83e2f41b6f5c44bc580eb95d
aman <aman.khan.silawat.aks@gmail.com>
2020-09-29 23:56:14 +0530

    Revision: basic closure memoization

--- revise.js
+++ revise.js
@@ -1,6 +1,6 @@
 // *basic memoization*
 function tenTime(n) {
-    n * 10;
+    return n * 10;
 }
 
 const cache = {};
@@ -24,10 +24,6 @@ basicMemo(6);
 // ....
 // .......
 // *basic function memoization*
-function tenTime(n) {
-    n * 10;
-}
-
 function funMemo(cb) {
     const cache = {};
     return function (n) {
@@ -46,5 +42,32 @@ function funMemo(cb) {
 let memo = funMemo(tenTime);
 memo(6); // new calculate
 memo(6); // finding from cache
+
+
+
+// ..
+// ....
+// .......
+// *basic closure memoization*
+
+function funClosureMemo(cb) {
+    const cache = {};
+    return function (n, ...args) {
+        if (n in cache) {
+            console.log('finding from cache');
+            return cache[n];
+        } else {
+            console.log('new calculate');
+            let res = cb(n, ...args);
+            cache[n] = res;
+            return res;
+        }
+    };
+}
+
+let memo2 = funClosureMemo(tenTime);
+memo2(8); // new calculate
+memo2(8); // finding from cache
+memo2(9); // new calculate
 
 
