76e58ab1a824a4c99cc0d46eef05877fd9343b0b
aman <aman.khan.silawat.aks@gmail.com>
2020-09-29 23:40:35 +0530

    Revision: basic function memoization

--- revise.js
+++ revise.js
@@ -9,9 +9,8 @@ function basicMemo(n) {
     if (n in cache) {
         console.log('finding from cache');
         return cache[n];
-    }
-    else {
-        console.log('new calculate')
+    } else {
+        console.log('new calculate');
         let res = tenTime(n);
         cache[n] = res;
         return res;
@@ -20,3 +19,32 @@ function basicMemo(n) {
 
 basicMemo(5);
 basicMemo(6);
+
+// ..
+// ....
+// .......
+// *basic function memoization*
+function tenTime(n) {
+    n * 10;
+}
+
+function funMemo(cb) {
+    const cache = {};
+    return function (n) {
+        if (n in cache) {
+            console.log('finding from cache');
+            return cache[n];
+        } else {
+            console.log('new calculate');
+            let res = cb(n);
+            cache[n] = res;
+            return res;
+        }
+    };
+}
+
+let memo = funMemo(tenTime);
+memo(6); // new calculate
+memo(6); // finding from cache
+
+
